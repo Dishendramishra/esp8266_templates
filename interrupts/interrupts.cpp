6b171ea4ed8c0d63bab51890ff5bb49da40301b5
Dishendra <dishendramishra@gmail.com>
2020-09-22 11:56:31 +0530

    added

---
+++ interrupts/interrupts.cpp
@@ -0,0 +1,26 @@
+
+#include <Arduino.h>
+
+const byte interruptpin = 0;
+
+
+
+void ICACHE_RAM_ATTR ISRoutine ();
+
+void setup () {
+ Serial.begin(115200);
+ pinMode(interruptpin,INPUT_PULLUP);
+ pinMode(LED_BUILTIN,OUTPUT);
+ attachInterrupt(digitalPinToInterrupt(interruptpin),ISRoutine,FALLING);
+
+}
+
+void loop () {
+
+ // Repeatative code here
+
+}
+
+void ISRoutine () {
+   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
